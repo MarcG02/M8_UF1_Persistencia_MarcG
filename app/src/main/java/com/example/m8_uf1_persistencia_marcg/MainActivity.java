202b2ba2a564ec3e4eba6f60c232e974c9432909
MarcG <marcgalaxy02@gmail.com>
2022-11-04 19:24:30 +0100

    Edit Text

--- app/src/main/java/com/example/m8_uf1_persistencia_marcg/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_persistencia_marcg/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.m8_uf1_persistencia_marcg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.idButton);
+
+        try  {
+            OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
+            String message = "Hola";
+            os.write(message.getBytes());
+            os.close();
+
+            File dir = this.getFilesDir();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
