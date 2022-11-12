e43a0546212e66a8c90108942f80f75e36dce23b
MarcG02 <marcgalaxy02@gmail.com>
2022-11-13 00:07:35 +0100

    View Implemented correctly and scrolleable. Final Commit

--- app/src/main/java/com/example/m8_uf1_persistencia_marcg/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_persistencia_marcg/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.m8_uf1_persistencia_marcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +17,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,17 +32,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView view = (TextView) findViewById(R.id.textView);
         EditText inputText = (EditText) findViewById(R.id.TextPersonName);
         Button button = findViewById(R.id.idButton);
+        String FileName = "dades.txt";
+        String text = "";
+
+        /*This line contains the file*/
+        String file = Arrays.toString(this.getFilesDir().list());
+
+        /* if file isn't exists this condition generates a new file*/
+        if (!file.contains("dades.txt")) {
+            File ruta = new File(this.getFilesDir() + File.separator + FileName);
+
+            try {
+                OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
+                /*Succsessful text input when the file is created*/
+                text = "File Created";
+                Toast.makeText(MainActivity.this, "File was succsessfuly created", Toast.LENGTH_SHORT).show();
+
+                os.write(text.getBytes());
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
         try  {
-            String FileName = "dades.txt";
             FileInputStream fileInS = openFileInput(FileName);
             InputStreamReader fileInR = new InputStreamReader(fileInS);
             BufferedReader buffReader = new BufferedReader(fileInR);
             String line = buffReader.readLine();
-            String text = "";
 
             while(line != null){
                 text += line + "\n";
@@ -46,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 line = buffReader.readLine();
             }
 
-            view.setText(text);
             buffReader.close();
             fileInR.close();
 
@@ -80,11 +103,13 @@ public class MainActivity extends AppCompatActivity {
                             os.flush();
                             os.close();
 
+                            updateView(FileName, text);
                         }
 
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+
                     Toast.makeText(MainActivity.this, "Text was written correctly", Toast.LENGTH_SHORT).show();
                     inputText.getText().clear();
 
@@ -92,6 +117,30 @@ public class MainActivity extends AppCompatActivity {
 
             });
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        updateView(FileName, text);
+    }
+    public void updateView(String FileName, String text) {
+        TextView view = (TextView) findViewById(R.id.textView2);
+        view.setMovementMethod(new ScrollingMovementMethod());
+        try {
+            FileInputStream fileInS = openFileInput(FileName);
+            InputStreamReader fileInR = new InputStreamReader(fileInS);
+            BufferedReader buffReader = new BufferedReader(fileInR);
+            String line = buffReader.readLine();
+            text = "";
+
+            while (line != null) {
+                text += line + "\n";
+
+                line = buffReader.readLine();
+            }
+
+            buffReader.close();
+            fileInR.close();
+            view.setText(text);
         } catch (IOException e) {
             e.printStackTrace();
         }
